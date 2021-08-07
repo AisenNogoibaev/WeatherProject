90f92a602ab4e0650d7e0a7698ffe4961bf6005b
Aisen Nogoibaev <aisennogoibaev999@gmail.com>
2021-08-07 23:22:26 +0600

    add_weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
+import React, { useState } from 'react'
+import Titles from './components/Titles'
+import Form from './components/Form'
+import Weather from './components/Weather'
 import './App.css'
+const APP_KEY = '62d5904a96356d22994c12327163e330'
 
 function App() {
-	return <div className='App'></div>
+	const [bd, setBd] = useState({
+		temperature: null,
+		city: null,
+		country: null,
+		humidity: null,
+		description: null,
+		error: null,
+	})
+
+	const getWeather = async (e) => {
+		e.preventDefault()
+		const city = e.target.elements.city.value
+		const api_call = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APP_KEY}&units=metric`,
+		)
+		const data = await api_call.json()
+
+		if (city && data.cod != 404) {
+			setBd({
+				temperature: data.main.temp,
+				city: data.name,
+				country: data.sys.country,
+				humidity: data.main.humidity,
+				description: data.weather[0].description,
+				error: '',
+			})
+		} else {
+			setBd({
+				temperature: undefined,
+				city: undefined,
+				country: undefined,
+				humidity: undefined,
+				description: undefined,
+				error: 'Please enter the correct value.',
+			})
+		}
+	}
+	return (
+		<div>
+			<div className='wrapper'>
+				<div className='main'>
+					<div className='container'>
+						<div className='row'>
+							<div className='col-6 title-container'>
+								<Titles />
+							</div>
+							<div className='col-6 form-container'>
+								<Form getWeather={getWeather} />
+								<Weather
+									temperature={bd.temperature}
+									city={bd.city}
+									country={bd.country}
+									humidity={bd.humidity}
+									description={bd.description}
+									error={bd.error}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
